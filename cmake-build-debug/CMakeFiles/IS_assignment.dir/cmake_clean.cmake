2bc1767f40f28064b8cfd3a02f2f9dcc6b6da505
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2020-05-15 22:22:06 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/IS_assignment.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IS_assignment.dir/Assignment_0/Ex00/simple_filter.c.o"
+  "IS_assignment"
+  "IS_assignment.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/IS_assignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
