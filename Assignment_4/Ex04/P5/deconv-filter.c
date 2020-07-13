c7f6002199af2195f827773ea1db02857bef8029
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2020-07-13 11:34:06 +0200

    init: initial commit for assignment04

---
+++ Assignment_4/Ex04/P5/deconv-filter.c
@@ -0,0 +1,27 @@
+/* ---------------------------------------------------------------------- */
+
+void filter
+
+     (float    **ur,        /* real part of Fourier coeffs, changed */
+      float    **ui,        /* imag. part of Fourier coeffs, changed */
+      float    **hr,        /* real part of Fourier kernel, unchanged */
+      float    **hi,        /* imag. part of Fourier kernel, unchanged */
+      float    param,       /* filter parameter */
+      long     nx,          /* pixel number in x direction */
+      long     ny)          /* pixel number in y direction */
+
+/*
+ allows to filter the Fourier coefficients
+*/
+
+{
+  long   i, j;              /* loop variables */
+  float  N;                 /* denominator */
+  float  vr,vi;             /* auxiliary variables for cplx arithm. */
+
+/* ---- compute filtered coefficients ---- */
+
+/** ---- INSERT CODE HERE ---- **/
+
+return;
+}
